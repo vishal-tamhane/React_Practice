ed37e32299bb81af7f0190a9318b4d80f4f1bac7
vishal-Tamhane <vishaltamhane367@gmail.com>
2025-06-07 20:35:32 +0530

    feat: initialize TodoContextLocal project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development and build.
    - Included Vite logo SVG in the public directory.
    - Created App component with basic structure for the To-Do List.
    - Added React logo SVG in assets directory.
    - Implemented Form component for adding new todos.
    - Developed Item component for displaying individual todo items with edit and delete functionality.
    - Set up TodoContext for managing todo state across components.
    - Configured Tailwind CSS in index.css for styling.
    - Established main entry point in main.jsx for rendering the application.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ 10TodoContextLocal/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import Form from './components/Form'
+import Item from './components/Item'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='p-4  m-10 bg-pink-700 text-black text-center'>To-Do List</h2>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                        <Form />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        <Item />
+                    </div>
+                </div>
+            </div>
+    </>
+  )
+}
+
+export default App
