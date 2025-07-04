5e484be399820e4cb7627e6cba68260196779f77
vishal-Tamhane <vishaltamhane367@gmail.com>
2025-07-04 20:36:45 +0530

    feat: integrate Tailwind CSS and set up Redux for authentication
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Refactored App component to include authentication logic using Redux.
    - Created Header and Footer components for layout structure.
    - Updated main entry point to include Redux Provider.
    - Removed existing CSS styles and replaced with Tailwind CSS import.

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,14 +1,42 @@
-
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import {Header,Footer} from "./components/index"
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading ,setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>Vishal tamhane appwrite blog app</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.currentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login(userData));
+      }
+      else{
+        dispatch(logout());
+      }
+    })
+    
+    .finally(() => setLoading(false));
+    }, [dispatch]);
+
+    return !loading ?(
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+         <div className='w-full block'>
+           <Header />
+           <main>
+            {/* TODo{outlet} */}
+           </main>
+           <Footer />
+         </div>
+      </div>
+    ): null
+
+  
 }
 
 export default App
